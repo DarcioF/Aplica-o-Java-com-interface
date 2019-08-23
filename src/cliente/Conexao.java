303941db21bf07a1244fc20ad7ba2cab7392d089
DarcioF <45544658+DarcioF@users.noreply.github.com>
2019-08-23 10:52:01 -0300

    Update Conexao.java

--- src/cliente/Conexao.java
+++ src/cliente/Conexao.java
@@ -10,13 +10,13 @@ public class Conexao {
     private static final String URL = "jdbc:mysql://localhost:3306/cliente";
     private static final String USER = "adm";
     private static final String PASS = "";    
-   //DriverManager.getConnection("jdbc:postgresql://localhost:5432/cliente","postgres", "amstopams");
+   //DriverManager.getConnection("jdbc:postgresql://localhost:5432/cliente","postgres", "");
            
     public Connection getConnection(){
 
         try {
             Class.forName("org.postgresql.Driver");          
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/cliente","postgres", "amstopams");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/cliente","postgres", "");
          
         } catch (ClassNotFoundException | SQLException ex) {
             throw new RuntimeException("Erro na conexao", ex);
